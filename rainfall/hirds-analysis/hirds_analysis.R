e067927f1cc8860f916c616019a7076b03d4bc7a
matt.ogden <mogd001@gmail.com>
2022-12-22 08:47:42 +1300

    Hirds analysis update

--- rainfall/hirds-analysis/hirds_analysis.R
+++ rainfall/hirds-analysis/hirds_analysis.R
@@ -43,6 +43,20 @@ nz_record <- tribble(
   120, 1368
 )
 
+nelson_tasman_record <- tribble(
+  ~duration, ~duration_label, ~value, ~site , ~event,
+  1 / 6, "10 min", 32, "HY Roding at Caretakers", "April 2013",
+  3 / 6, "30 min", 62, "HY Roding at Caretakers", "April 2013",
+  1, "1 hour", 104, "HY Roding at Caretakers", "April 2013",
+  2, "2 hours", 138.5, "HY Waimea at TDC Nursery", "April 2013",
+  6, "6 hours", 218.4, "HY Takaka at Harwoods", "February 2018",
+  12, "12 hours",  343.1, "HY Anatoki at Paradise", "February 2016",
+  24, "1 day", 493.5, "HY Bainham at Langfords Store", "April 1931",
+  48, "2 days", 742.8, "HY Anatoki at Paradise", "August 2022",
+  72, "3 days", 834.7, "HY Anatoki at Paradise", "August 2022",
+  120, "5 days", 1058, "HY Anatoki at Paradise", "August 2022"
+)
+
 hirds <- hirds_table %>%
   mutate(ari = fct_rev(factor(ari))) %>%
   pivot_longer(!ari, names_to = "duration_text", values_to = "val") %>%
@@ -66,30 +80,38 @@ dplyr::select(observed, c(duration_label, rainfall_mm = value, ari_for_duration)
                 save_kable("temp_table.html")
 
 # Plot 1
-arrows <- 
-  tibble(
-    x1 = c(6),
-    x2 = c(24),
-    y1 = c(1000), 
-    y2 = c(800)
-  )
+arrow_nz <- tibble(x1 = c(12), x2 = c(23.5), y1 = c(2000), y2 = c(820))
+arrow_nt <- tibble(x1 = c(1/2), x2 = c(5/6), y1 = c(400), y2 = c(95))
 
 p1 <- ggplot() +
   geom_line(hirds, mapping = aes(duration, val, color = ari), linetype = "dashed") +
-  geom_line(observed, mapping = aes(duration, value), color = "magenta", size = 1) +
-  geom_line(nz_record, mapping = aes(duration, value), color = "red", size = 1) +
+
+  geom_line(nz_record, mapping = aes(duration, value), color = "red", size = 1, alpha = 0.5) +
+  geom_point(nz_record, mapping = aes(duration, value), color = "red", size = 2) +
+  geom_line(nelson_tasman_record, mapping = aes(duration, value), color = "orange", size = 1, alpha = 0.5) +
+  geom_point(nelson_tasman_record, mapping = aes(duration, value), color = "orange", size = 2) +
   
+  geom_text(nelson_tasman_record, mapping = aes(duration, value, label = glue("{substring(site, 4)} {event}")), color = "orange", size = 2, angle = 75, hjust = -0.1, vjust = 0) + 
   
   geom_curve(
-    data = arrows, aes(x = x1, y = y1, xend = x2, yend = y2),
+    data = arrow_nz, aes(x = x1, y = y1, xend = x2, yend = y2),
     size = 0.5,
     arrow = arrow(length = unit(0.08, "inch")),
     color = "red", curvature = -0.1) +
-  geom_label(aes(x = 6, y = 1000, label = "NZ Record"), color = "red", fill = "white") +
+  geom_label(aes(x = 12, y = 2000, label = "NZ Record"), color = "red", fill = "white") +
   
+  geom_curve(
+    data = arrow_nt, aes(x = x1, y = y1, xend = x2, yend = y2),
+    size = 0.5,
+    arrow = arrow(length = unit(0.08, "inch")),
+    color = "orange", curvature = -0.2) +
+  geom_label(aes(x = 1/2, y = 400, label = "Nelson Tasman Record"), color = "orange", fill = "white") +
+  
+  geom_line(observed, mapping = aes(duration, value), color = "magenta", size = 1) +
   geom_point(observed, mapping = aes(duration, value), color = "magenta", size = 2.5) +
+  
   scale_x_continuous(trans = "log10", breaks = duraction_text_to_val$duration, labels = duraction_text_to_val$duration_label) +
-  scale_y_log_eng() +
+  scale_y_log_eng(limits = c(7, 2900)) +
   scale_color_viridis_d() +
   labs(
     x = "Duration", y = "Rainfall (mm)", color = "ARI (years)", linetype = NULL,
@@ -112,10 +134,13 @@ p1 <- ggplot() +
   theme_bw() +
   theme(
     axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    plot.title = element_text(colour = "magenta"),
     panel.grid.minor.x = element_blank()
   )
 
 p1
+ggsave("hirds_output_example.png", plot = p1, dpi = 300, height = 8, width = 12)
+
 ggplotly(p1)
 htmlwidgets::saveWidget(ggplotly(p1), "hirds_output_example.html")
 
