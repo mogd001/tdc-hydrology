80db7bc993a344abd6ff854632821635ee9c8106
matt.ogden <mogd001@gmail.com>
2023-04-04 15:12:08 +1200

    Updated scheduled task

--- hydro_reporting_scheduled_task.R
+++ hydro_reporting_scheduled_task.R
@@ -1,12 +1,25 @@
 library(glue)
+library(lubridate)
 
 src_dir <- "C:/Users/matto/OneDrive - Tasman District Council/Desktop/Working/R/tdc-hydrology"
 dst_dir <- "M:/Processing/R_Outputs"
 
 # Clean final outputs folder
 unlink(glue("{dst_dir}/*"), recursive = TRUE)
-#dir.create(paste0(dst_dir, "/rainfall"))
-#dir.create(paste0(dst_dir, "/flow"))
+
+## rainfall past 24 hours
+
+setwd(src_dir)
+setwd("rainfall/rainfall-summary-p24hrs")
+
+source("rainfall_summary_p24hrs.R")
+
+fs <- list.files(paste0(getwd(),"/outputs"))
+file.copy(paste0(getwd(),"/outputs/", fs), dst_dir, recursive = TRUE)
+
+d <- now()
+# Archive
+arch_dirname <- format(d, "%Y%m%d-%H")
 
 ### rainfall sparklines
 
@@ -15,12 +28,25 @@ setwd("rainfall/sparklines")
 
 source("rainfall_sparklines.R")
 
-file.copy(paste0(getwd(),"/rainfall_outputs"), dst_dir, recursive= TRUE)
+fs <- list.files(paste0(getwd(),"/rainfall-sparklines"))
+file.copy(paste0(getwd(),"/rainfall-sparklines/", fs), dst_dir, recursive = TRUE)
 
 ### flow sparklines
+
 setwd(src_dir)
 setwd("flow/sparklines")
 
 source("flow_sparklines.R")
 
-file.copy(paste0(getwd(),"/flow_outputs"), dst_dir, recursive= TRUE)
+fs <- list.files(paste0(getwd(),"/flow-sparklines"))
+file.copy(paste0(getwd(),"/flow-sparklines/", fs), dst_dir, recursive = TRUE)
+
+## rainfall past 7 days
+
+setwd(src_dir)
+setwd("rainfall/rainfall-summary-p7days")
+
+source("rainfall_summary_p7days.R")
+
+fs <- list.files(paste0(getwd(),"/outputs"))
+file.copy(paste0(getwd(),"/outputs/", fs), dst_dir, recursive = TRUE)
