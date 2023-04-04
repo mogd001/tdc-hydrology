a57edc4f5bd9c9a1b14bfcbc9a846220108d8af1
matt.ogden <mogd001@gmail.com>
2023-04-04 15:03:35 +1200

    Remove event reporting from repo

--- event-reporting/new_event_summary_in_development.R
+++
@@ -1,6 +0,0 @@
-# Clear outputs folder
-if (dir.exists("./outputs")) {
-  unlink("./outputs", recursive = TRUE)
-  Sys.sleep(2) # give time for the removal of the directory to take place
-}
-dir.create("./outputs")
