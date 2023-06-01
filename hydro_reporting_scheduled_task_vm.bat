143fafc185930aa6d246b85b010f9f06bf1895e3
matt.ogden <mogd001@gmail.com>
2023-06-02 09:44:30 +1200

    Add vm script and bat file

---
+++ hydro_reporting_scheduled_task_vm.bat
@@ -0,0 +1,3 @@
+REM @Echo Off
+REM "C:\Program Files\R\R-4.3.0\bin\R.exe" CMD BATCH C:\Apps\Hydro\tdc-hydrology\hydro_reporting_scheduled_task_vm.R
+"C:\Program Files\R\R-4.3.0\bin\Rscript.exe" C:\Apps\Hydro\tdc-hydrology\hydro_reporting_scheduled_task_vm.R
